c2b0e0f9865082a86ba8cd7e6c74057daecc7294
K1000o-Lp <camiloalfonsolopezparra@gmail.com>
2022-02-03 15:13:52 -0400

    Se creó la clase VentaDAO

---
+++ MiniProyecto/src/dao/VentaDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.*;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class VentaDAO {
+    
+    private Statement statement;
+    private Connection conexion;
+    private ResultSet resultSet;
+    private DefaultTableModel modelo;
+    private DataBase db;
+    
+    public VentaDAO() {
+    
+        db = new DataBase();
+    }
+    
+    
+}
