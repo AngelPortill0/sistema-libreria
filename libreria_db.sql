2495353a6f005bb9a345472f41e2f8861832bd16
K1000o-Lp <camiloalfonsolopezparra@gmail.com>
2022-02-06 18:42:23 -0400

    Corrección de duplicado en db

--- libreria_db.sql
+++ libreria_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 06, 2022 at 08:34 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-02-2022 a las 23:41:22
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,39 +18,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `libreria_db`
+-- Base de datos: `libreria_db`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `autor`
+-- Estructura de tabla para la tabla `autor`
 --
 
 CREATE TABLE `autor` (
-  `id` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
   `nombre` varchar(20) NOT NULL,
   `apellido` varchar(20) NOT NULL,
   `fechaDeNacimiento` date NOT NULL,
-  `biografia` varchar(500) NOT NULL,
+  `biografia` varchar(1000) NOT NULL,
   `numeroDePublicaciones` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `autor`
+-- Volcado de datos para la tabla `autor`
 --
 
 INSERT INTO `autor` (`id`, `nombre`, `apellido`, `fechaDeNacimiento`, `biografia`, `numeroDePublicaciones`) VALUES
-(4, 'Mark', 'Douglas', '1968-10-20', 'Mark Douglas es un trader rentable que busca adquirir y fomentar conocimiento referente a todo lo relacionado con el trading, como la psicología, estrategias, gestión de riesgo y análisis técnico.', 20),
-(5, 'Alfredo', 'Chaumer', '1978-02-02', 'Alfredo nació en Caracas, Venezuela. Sus estudios los realizó en la UCV como sociólogo.', 1),
-(6, 'Gabriel José', 'García Márquez', '1927-03-06', 'Gabriel José García Márquez    fue un escritor y periodista colombiano. Reconocido principalmente por sus novelas y cuentos, también escribió narrativa de no ficción, discursos, reportajes, críticas cinematográficas y memorias. Fue conocido como Gabo, y familiarmente y por sus amigos como Gabito.', 48),
-(15, 'Jane', 'Austen', '1775-12-16', 'Fue una novelista británica que vivió durante la época georgiana. La ironía que empleaba para dotar de comicidad a sus novelas hace que Jane Austen sea considerada entre los clásicos de la novela inglesa,  a la vez que su recepción va, incluso en la actualidad, más allá del interés académico, siendo sus obras leídas por un público más amplio', 30),
-(16, 'Jorge', 'Borges', '1889-07-24', 'Jorge Francisco Isidoro Luis Borges, más conocido como Jorge Luis Borges, fue un destacado escritor de cuentos, poemas y ensayos argentino, extensamente considerado una figura clave tanto para la literatura en habla hispana como para la literatura universal.', 80);
+(1, 'Alfredo', 'Chaumer', '1978-02-02', 'Alfredo José Chaumer Herrera Nació en Caracas, Venezuela. Graduado de Sociólogo egresado de la Universidad Central de Venezuela en el año 2009; curso estudios de Planificación del desarrollo y se ha especializado en el análisis de datos para la toma de decisiones. Consultor, asesor y Trader desde el año 2012, su proceso de aprendizaje siempre se ha enriquecido transmitiendo pedagógicamente la sistematización de su experiencia, es por ello que en el año 2018 crea un programa de capacitación integral enfocado en el desarrollo de un criterio propio y en estimular la consistencia operativa siempre enfocado en la rentabilidad factible.', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genero`
+-- Estructura de tabla para la tabla `genero`
 --
 
 CREATE TABLE `genero` (
@@ -59,25 +55,21 @@ CREATE TABLE `genero` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `genero`
+-- Volcado de datos para la tabla `genero`
 --
 
 INSERT INTO `genero` (`id`, `genero`) VALUES
-(2, 'Negocios y Economía'),
-(3, 'Novela'),
-(4, 'Obra de Teatro'),
-(5, 'Cuento'),
-(6, 'Cuento');
+(1, 'Conocimientos');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `libro`
+-- Estructura de tabla para la tabla `libro`
 --
 
 CREATE TABLE `libro` (
   `id` int(10) NOT NULL,
-  `titulo` varchar(200) NOT NULL,
+  `titulo` varchar(20) NOT NULL,
   `fechaDePublicacion` date NOT NULL,
   `isbn` varchar(50) NOT NULL,
   `numeroDePaginas` int(255) NOT NULL,
@@ -87,19 +79,16 @@ CREATE TABLE `libro` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `libro`
+-- Volcado de datos para la tabla `libro`
 --
 
 INSERT INTO `libro` (`id`, `titulo`, `fechaDePublicacion`, `isbn`, `numeroDePaginas`, `puntuacion`, `descripcion`, `precio`) VALUES
-(14, 'Ficciones', '1944-01-01', '13:9789875666474', 250, 7, 'Ficciones es un libro de cuentos escrito por Jorge Luis Borges, publicado en 1944 y compuesto de dos partes: El jardín de senderos que se bifurcan y Artificios; posee dos prólogos.', 520),
-(15, 'Lady Susan', '1870-01-01', '13:9780140431025', 150, 7, 'Lady Susan, es una novela corta epistolar de Jane Austen. Posiblemente escrita en 1794, fue publicada en 1871 en forma póstuma.', 90),
-(16, 'Emma', '1815-12-23', '9789588925769', 50, 10, 'Emma es una novela cómica escrita por Jane Austen, en inglés, publicada por vez primera en 1815 por el editor John Murray, sobre los peligros de malinterpretar el romance. El personaje principal, Emma Woodhouse, se describe como \"hermosa, lista y rica\" pero también es bastante mimada.', 5.99),
-(17, 'Orgullo y prejuicio', '1813-01-28', '9780192561411', 90, 58, 'La novela describe poco más de un año en la vida de un pequeño grupo de jóvenes en el campo cerca de Londres en el cambio de siglo (del XVIII al XIX), durante el reinado de Jorge III.', 7);
+(1, 'Trading como Negocio', '2020-04-01', 'B086PG66L2', 101, 10, 'Mucho se ha escrito sobre trading, innumerables libros enfocados en operativas, análisis, estrategias, incluso en tratar de diagramar paso a paso esta actividad. En este caso, busco trascender de lo técnico o esperanzador y me enfoco en estimular el proceso reflexivo que permite configurar una percepción del trading propia. A través de diversos elementos, enfoques o concepciones busco exponer los principales escenarios en los cuales contextualizamos el trading, las limitaciones particulares que más se repiten, acciones orientadas a superar circunstancias complejas, para luego plantear una concepción factible y exitosa con la cual he logrado desarrollar resultados rentables desde hace más de 3 años.\r\nEscribí este libro no con ínfulas ni pretensiones de experto financiero, y creo que ya es evidente que no te estoy ofreciendo una operativa, una fórmula, una entrada o un método mágico para crear riqueza de forma instantánea y segura. Mi promesa para ti, cuando termines de leer este libro, es que podrás ver el trading desde una perspectiva que trasciende el “marketing esperanzador”, un paradigma integrador y de negocios, lo que te permitirá diseñar esta actividad a tu medida, con una visión a largo plazo, utilizando todas las herramientas de las cuales dispongas y adquiriendo todo el conocimiento que consideres apropiado para hacer del trading un negocio rentable.', 9.99);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `libros_autores`
+-- Estructura de tabla para la tabla `libros_autores`
 --
 
 CREATE TABLE `libros_autores` (
@@ -109,44 +98,36 @@ CREATE TABLE `libros_autores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `libros_autores`
+-- Volcado de datos para la tabla `libros_autores`
 --
 
 INSERT INTO `libros_autores` (`idLibro`, `idGenero`, `idAutor`) VALUES
-(16, 3, 15),
-(16, 3, 15),
-(14, 5, 16),
-(14, 6, 16),
-(15, 3, 15),
-(17, 3, 15),
-(14, 2, 6),
-(17, 4, 6),
-(15, 6, 4);
+(1, 1, 1);
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `autor`
+-- Indices de la tabla `autor`
 --
 ALTER TABLE `autor`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `genero`
+-- Indices de la tabla `genero`
 --
 ALTER TABLE `genero`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `libro`
+-- Indices de la tabla `libro`
 --
 ALTER TABLE `libro`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `libros_autores`
+-- Indices de la tabla `libros_autores`
 --
 ALTER TABLE `libros_autores`
   ADD KEY `idLibro` (`idLibro`),
@@ -154,38 +135,38 @@ ALTER TABLE `libros_autores`
   ADD KEY `idAutor` (`idAutor`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `autor`
+-- AUTO_INCREMENT de la tabla `autor`
 --
 ALTER TABLE `autor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `genero`
+-- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `libro`
+-- AUTO_INCREMENT de la tabla `libro`
 --
 ALTER TABLE `libro`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `libros_autores`
+-- Filtros para la tabla `libros_autores`
 --
 ALTER TABLE `libros_autores`
   ADD CONSTRAINT `libros_autores_ibfk_1` FOREIGN KEY (`idGenero`) REFERENCES `genero` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `libros_autores_ibfk_2` FOREIGN KEY (`idAutor`) REFERENCES `autor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `libros_autores_ibfk_3` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `libros_autores_ibfk_2` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `libros_autores_ibfk_3` FOREIGN KEY (`idAutor`) REFERENCES `autor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
